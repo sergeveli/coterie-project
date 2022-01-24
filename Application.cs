64a8a6237897f336eee32621531a4c4a9e2748de
sergeveli <70241892+sergeveli@users.noreply.github.com>
2022-01-23 19:49:50 -0500

    starting off on api

---
+++ Application.cs
@@ -0,0 +1,20 @@
+namespace coterie_project;
+
+public class Applications
+{
+    public string BusinessName { get; set; }
+
+    public string Industry { get; set; }
+
+    public string Email {get; set;}
+
+    public int AnnualSales { get; set; }
+
+    public int AnnualPayroll { get; set; }
+
+
+    public int Employees {get; set;}
+
+    public string ZipCode {get; set;}
+
+}
