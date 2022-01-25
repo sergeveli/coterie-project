a644e9f16a9e4a6fb88034d4144140df22f00547
sergeveli <70241892+sergeveli@users.noreply.github.com>
2022-01-24 19:11:42 -0500

    front end started

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { store } from './actions/store';
+import {Provider} from 'react-redux'
+
+function App() {
+  return (
+    <Provider store={store}>
+      
+    </Provider>
+  );
+}
+
+export default App;
