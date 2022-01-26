6cc7d47ea59f6697612839086f91a40b00cc948f
sergeveli <70241892+sergeveli@users.noreply.github.com>
2022-01-26 02:10:05 -0500

    cors added but check documentation, still buggy

--- Program.cs
+++ Program.cs
@@ -6,8 +6,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
 
 var app = builder.Build();
+
+
+app.UseCors(options =>
+options.WithOrigins("http://localhost:3000")
+.AllowAnyHeader()
+.AllowAnyMethod());
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
